df18c26812deb3af05e582d19aa38b196b6c2006
JamesZam96 <jameszamgm@gmail.com>
2021-11-12 21:33:51 -0500

    First Commit

---
+++ frontend_inventario/src/App.js
@@ -0,0 +1,17 @@
+import SideBar from "./components/sidebar/SideBar";
+import TopBar from "./components/topbar/TopBar";
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+      <div className="container">
+        <SideBar/>
+        <div className="others">other pages</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend_inventario/src/components/topbar/TopBar.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import './TopBar.css'
+import {NotificationsNone, Language, Settings} from '@material-ui/icons';
+
+export default function TopBar(){
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">InStore</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsNone/>
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Language/>
+                        <span className="topIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings/>
+                    </div>
+                    <img src="https://images.pexels.com/photos/4483610/pexels-photo-4483610.jpeg?cs=srgb&dl=pexels-tiger-lily-4483610.jpg&fm=jpg" alt="" className="topAvatar"/>
+                </div>
+            </div>
+        </div>
+    )
+}
