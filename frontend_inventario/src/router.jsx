bce915e483c35aa362424594f9e24bb7d69809a2
Luisa Fernanda Gutiérrez Acosta <fernandagac@gmail.com>
2021-11-16 15:25:20 -0600

    registerEmployee page created

--- frontend_inventario/src/router.jsx
+++ frontend_inventario/src/router.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import App from './App'
 import Login from './components/auth/login/Login'
 import Register from './components/auth/register/Register'
+import Employee from './components/auth/register/employee';
 export default function Router() {
     return(
         <Routes>
             <Route path={'/'} element={<App />} />
             <Route path={'/login'} element={<Login />} />
             <Route path={'/register'} element={<Register />} />
+            <Route path={'/registerEmployee'} element={<Employee />} />
         </Routes>
     )
 }
