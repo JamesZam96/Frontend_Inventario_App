48bd051340b02267952ae1c490211e6237b487d8
Luisa Fernanda Gutiérrez Acosta <fernandagac@gmail.com>
2021-12-02 22:06:52 -0600

    make it better

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router.jsx'
+import axios from 'axios'
 // import App from './App';
 require('dotenv').config()
 
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
 ReactDOM.render(
   // <React.StrictMode>
